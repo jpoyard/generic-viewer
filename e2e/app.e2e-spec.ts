0f582c00ec81b0feda310bb172b95cc554d62604
Angular CLI <angular-cli@angular.io>
2017-06-19 20:57:02 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GenericViewerPage } from './app.po';
+
+describe('generic-viewer App', () => {
+  let page: GenericViewerPage;
+
+  beforeEach(() => {
+    page = new GenericViewerPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
