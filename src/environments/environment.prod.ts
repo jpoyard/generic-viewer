ff72c7658dcaaee626e6d26be6458698fb156d33
jpoyard <jpoyard@oxiane.com>
2017-06-19 21:37:14 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Initialize Firebase
+  firebase: {
+    apiKey: 'AIzaSyA6fKrhz9U-b6T-RtUCo3JxA3PsrMG24DI',
+    authDomain: 'genericviewer-aeee9.firebaseapp.com',
+    databaseURL: 'https://genericviewer-aeee9.firebaseio.com',
+    projectId: 'genericviewer-aeee9',
+    storageBucket: 'genericviewer-aeee9.appspot.com',
+    messagingSenderId: '69297115546'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Initialize Firebase
+  firebase: {
+    apiKey: 'AIzaSyA6fKrhz9U-b6T-RtUCo3JxA3PsrMG24DI',
+    authDomain: 'genericviewer-aeee9.firebaseapp.com',
+    databaseURL: 'https://genericviewer-aeee9.firebaseio.com',
+    projectId: 'genericviewer-aeee9',
+    storageBucket: 'genericviewer-aeee9.appspot.com',
+    messagingSenderId: '69297115546'
+  }
 };
