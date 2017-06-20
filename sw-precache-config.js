2c9530d372927fe826b85667d072fb49759dcfaa
jpoyard <jpoyard@oxiane.com>
2017-06-20 22:41:24 +0200

    implements progressive web app structure

---
+++ sw-precache-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    staticFileGlobs: [
+        'dist/**.html',
+        'dist/**.js',
+        'dist/**.css',
+        'dist/**.ico',
+        'dist/assets/images/*',
+        'dist/assets/icons/*'
+    ],
+    root: 'dist',
+    stripPrefix: 'dist/',
+    navigateFallback: '/index.html',
+    runtimeCaching: [{
+        urlPattern: /node-hnapi\.herokuapp\.com/,
+        handler: 'networkFirst'
+    }]
+};
